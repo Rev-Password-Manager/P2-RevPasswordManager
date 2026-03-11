c2b1769de0f2573641abe86594601baba28b7734
Shiva01819 <shiva1462002@gmail.com>
2026-03-11 06:42:41 +0530

    updated repositories

--- P2-RevPasswordManager2/src/main/java/com/passwordmanager/repository/SecurityQuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.passwordmanager.repository;
-
-import com.passwordmanager.entity.SecurityQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SecurityQuestionRepository extends JpaRepository<SecurityQuestion, Long> {
-}
