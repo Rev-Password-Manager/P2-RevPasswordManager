aaeb833377031497828f96cdb4605b45dc011e50
Shiva Kumar <shiva1462002@gmail.com>
2026-03-11 09:25:38 +0530

    added audit controller

---
+++ src/test/java/com/passwordmanager/P2RevPasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2RevPasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
